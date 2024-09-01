8fe3c18b18b3fd11447b2c1ddb15698059967460
busragedikoglu <bgedikogluu@gmail.com>
2024-09-01 15:55:32 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Büşra Gedikoğlu on 1.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
